a8c4eb21d21dda8327cd2a2dc977014713593c92
Arthur <1horyuarthur@gmail.com>
2026-04-13 09:56:19 -0300

    chore: replace favicons with new assets and ensure title is updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,25 +39,12 @@ export const metadata: Metadata = {
     icon: [
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
-      { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
-      { url: '/favicon-57x57.png', sizes: '57x57', type: 'image/png' },
-      { url: '/favicon-60x60.png', sizes: '60x60', type: 'image/png' },
-      { url: '/favicon-72x72.png', sizes: '72x72', type: 'image/png' },
-      { url: '/favicon-76x76.png', sizes: '76x76', type: 'image/png' },
-      { url: '/favicon-114x114.png', sizes: '114x114', type: 'image/png' },
-      { url: '/favicon-120x120.png', sizes: '120x120', type: 'image/png' },
-      { url: '/favicon-144x144.png', sizes: '144x144', type: 'image/png' },
-      { url: '/favicon-152x152.png', sizes: '152x152', type: 'image/png' },
-      { url: '/favicon-180x180.png', sizes: '180x180', type: 'image/png' },
-    ],
-    other: [
-      {
-        rel: 'apple-touch-icon-precomposed',
-        url: '/favicon-152x152.png',
-      },
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     ],
   },
 };
